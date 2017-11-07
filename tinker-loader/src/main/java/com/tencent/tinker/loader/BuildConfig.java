af55dbaf02064220f2e20153d8e6035d5ef1d404
shaopengxiang <shaopengxiang@sogou-inc>
2017-11-07 19:01:18 +0800

    Initial commit

---
+++ tinker-loader/src/main/java/com/tencent/tinker/loader/BuildConfig.java
@@ -0,0 +1,14 @@
+package com.tencent.tinker.loader;
+
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+    public static final String APPLICATION_ID = "com.tencent.tinker.loader";
+    public static final String BUILD_TYPE = "debug";
+    public static final String FLAVOR = "";
+    public static final int VERSION_CODE = -1;
+    public static final String VERSION_NAME = "";
+    // Fields from default config.
+    public static final String TINKER_VERSION = "1.9.0";
+}
+
